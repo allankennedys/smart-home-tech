03708e39be03451352f0e67ef66eb5ef0066b305
Lucas-Kevyn <lkevynpa@gmail.com>
2025-05-25 16:59:34 -0300

    Finalizei o form

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ function openModal() {
     modal.style.display = 'block';
     const formArea = document.getElementById('modalForm');
 
-    formArea.innerHTML = 'MONTE AQUI O FORMULÁRIO DO FORM SUBMIT'; 
+    formArea.innerHTML = `
+      <form action="https://formsubmit.co/lkevynpa@gmail.com" method="POST">
+        <input type="text" name="name" placeholder="Nome" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="tel" name="phone" placeholder="Telefone" required><br>
+        <textarea name="message" placeholder="Digite sua mensagem" required></textarea><br>
+        <input type="hidden" name="_next" value="http://127.0.0.1:5500/obrigado.html" required>
+        <button type="submit">Send</button>
+      </form>
+  `; 
   //dentro das aspas você vai montar o formulário de acordo com o tutorial/documentação do formSubmit      
 //Não precisa se preocupar com estilização, o importante é o formulário funcionar
 // O usuário precisa preencher o nome, e-mail e telefone e deve ter um botão de envio
