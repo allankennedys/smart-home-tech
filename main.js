cb2f4adc683584050afcadcbb250fb0eb8246e6c
allankennedys <kennedyyemail@gmail.com>
2025-05-27 01:16:57 -0300

    popup

--- main.js
+++ main.js
@@ -8,16 +8,18 @@ function openModal() {
     const formArea = document.getElementById('modalForm');
 
     formArea.innerHTML = `
+      <h3>Solicite um Orçamento</h3>
+      <p>Preencha o formulário abaixo e entraremos em contato o mais breve possível.</p>
       <form action="https://formsubmit.co/lkevynpa@gmail.com" method="POST">
         <input type="text" name="name" placeholder="Nome" required><br>
         <input type="email" name="email" placeholder="Email" required><br>
         <input type="tel" name="phone" placeholder="Telefone" required><br>
-        <textarea name="message" placeholder="Digite sua mensagem" required></textarea><br>
-        <input type="hidden" name="_next" value="http://127.0.0.1:5500/obrigado.html" required>
-        <button type="submit">Send</button>
+        <textarea name="message" placeholder="Digite sua mensagem (Opcional)"></textarea><br>
+        <input type="hidden" name="_next" value="obrigado.html" required>
+        <div id="submitArea">
+        <button type="submit" id="submit" >Solicitar</button>
+        <button type="button" onclick="closeModal()">Fechar</button>
+        </div>
       </form>
-  `; 
-  //dentro das aspas você vai montar o formulário de acordo com o tutorial/documentação do formSubmit      
-//Não precisa se preocupar com estilização, o importante é o formulário funcionar
-// O usuário precisa preencher o nome, e-mail e telefone e deve ter um botão de envio
+  `;
 }
