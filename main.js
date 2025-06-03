7388160bbcc5c09c01ab3ba064c6a3af56d4a4de
allankennedys <kennedyyemail@gmail.com>
2025-06-03 10:25:53 -0300

    placeholder remove

--- main.js
+++ main.js
@@ -15,11 +15,11 @@ function openModal() {
       <p>Preencha o formulário abaixo e entraremos em contato o mais breve possível.</p>
       <form action="https://formsubmit.co/lkevynpa@gmail.com" method="POST">
       <label for="name">Nome:</label>
-        <input type="text" name="name" placeholder="Nome" required style="width:100%; margin-bottom:10px;"><br>
+        <input type="text" name="name"required style="width:100%; margin-bottom:10px;"><br>
         <label for="email">Email:</label>
-        <input type="email" name="email" placeholder="Email" required style="width:100%; margin-bottom:10px;"><br>
+        <input type="email" name="email" required style="width:100%; margin-bottom:10px;"><br>
         <label for="phone">Telefone:</label>
-        <input type="tel" name="phone" placeholder="Telefone" required style="width:100%; margin-bottom:10px;"><br>
+        <input type="tel" name="phone" required style="width:100%; margin-bottom:10px;"><br>
         <textarea name="message" placeholder="Digite sua mensagem (Opcional)" style="width:100%; margin-bottom:10px;"></textarea><br>
         <input type="hidden" name="_captcha" value="false">
         <div id="submitArea" style="display:flex; justify-content:space-between;">
