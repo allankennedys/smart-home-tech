2c1ca6c1484124406b8d0de115d10d5d883d72ec
allankennedys <kennedyyemail@gmail.com>
2025-06-03 10:11:45 -0300

    labels, scroll behavior

--- main.js
+++ main.js
@@ -14,8 +14,11 @@ function openModal() {
       <h3>Solicite um Orçamento</h3>
       <p>Preencha o formulário abaixo e entraremos em contato o mais breve possível.</p>
       <form action="https://formsubmit.co/lkevynpa@gmail.com" method="POST">
+      <label for="name">Nome:</label>
         <input type="text" name="name" placeholder="Nome" required style="width:100%; margin-bottom:10px;"><br>
+        <label for="email">Email:</label>
         <input type="email" name="email" placeholder="Email" required style="width:100%; margin-bottom:10px;"><br>
+        <label for="phone">Telefone:</label>
         <input type="tel" name="phone" placeholder="Telefone" required style="width:100%; margin-bottom:10px;"><br>
         <textarea name="message" placeholder="Digite sua mensagem (Opcional)" style="width:100%; margin-bottom:10px;"></textarea><br>
         <input type="hidden" name="_captcha" value="false">
